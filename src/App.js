65b7b08e1288cc3369a1e65177af34fe420699f6
Jamison Polackwich <jamisonpolackwich@Jamisons-MacBook-Pro.local>
2022-12-14 13:01:41 -0700

    using routes

--- src/App.js
+++ src/App.js
@@ -1,55 +1,28 @@
-// <a href="https://www.freepik.com/free-vector/animal-heads-set-flat-style_4643514.htm#query=animal%20avatars&position=49&from_view=keyword">Image by macrovector_official</a> on Freepik
 import React, { Component } from 'react';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LoginForm } from './Login';
 import { OnlinePLayers } from './Lobby';
+import { io } from "socket.io-client";
 
 
-var src = "https://react.semantic-ui.com/images/avatar/small/rachel.png"; 
+const App = () => {
+	const socket = io.connect("http://localhost:4000");
+
+	return (
+		<> 
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<Login socket={socket} />} />
+					<Route path='/register' element={<Register socket={socket} />} />
+					<Route path='/lobby' element={<Lobby socket={socket} />} />
+
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
+};
+
+export default App;
 
 
-
-
-
-class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			onlinePlayers: [],
-			name: '',
-			email: '',
-			avatar: '',
-		};
-	}
-    onLogin = (loginInput) => {
-        this.setState({ username: loginInput })
-    }
-    
-
-	
-	onThrowChange(event) {
-		this.setState({
-			selectedThrow: event.target.value
-		});
-	}
-
-	throwSubmit(event) {
-		event.preventDefault();
-		console.log(this.state.selectedThrow)
-	}
-    
-    render() {
-        const { username } = this.state;
-        return (
-            <React.Fragment>
-                <Navbar color="light" light>
-                    <NavbarBrand href="/">Real-time document editor</NavbarBrand>
-                </Navbar>
-                <div className="container-fluid">
-                    {username ? <Lobby/> : <LoginForm onSubmit={this.onLogin.bind(this)}/>}
-                </div>
-            </React.Fragment>
-        );
-    }
-}
 
