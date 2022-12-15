085435dcc2885db14f43dd21fd0b8d186675812c
Jamison Polackwich <jamisonpolackwich@Jamisons-MacBook-Pro.local>
2022-12-14 17:52:09 -0700

    minimal functioning login

--- src/components/Lobby.js
+++ src/components/Lobby.js
@@ -1,23 +1,31 @@
 import React from "react";
-import { Image, List } from "semantic-ui-react";
+// import { Image, List } from "semantic-ui-react";
 
-const PlayerItem = ({ item }) => {
+// const PlayerItem = ({ item }) => {
+//   return (
+//     <List.Item>
+//       <Image avatar src={item.avlink} />
+//       <List.Content>
+//         <List.Header>{item.name}</List.Header>
+//       </List.Content>
+//     </List.Item>
+//   );
+// };
+
+// export const OnlinePlayers = ({ onlinePlayers }) => {
+//   return (
+//     <List selection verticalAlign="middle">
+//       {onlinePlayers.map((player) => (
+//         <PlayerItem key={player.id} item={player} />
+//       ))}
+//     </List>
+//   );
+// };
+
+const Lobby = ({ props }) => {
   return (
-    <List.Item>
-      <Image avatar src={item.avlink} />
-      <List.Content>
-        <List.Header>{item.name}</List.Header>
-      </List.Content>
-    </List.Item>
+    <h1>Hello</h1>
   );
 };
 
-export const OnlinePlayers = ({ onlinePlayers }) => {
-  return (
-    <List selection verticalAlign="middle">
-      {onlinePlayers.map((player) => (
-        <PlayerItem key={player.id} item={player} />
-      ))}
-    </List>
-  );
-};
+export default Lobby;
